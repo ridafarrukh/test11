16f4f8f6d1cbd9b8e5f27b91dbf7ca1931e878da
ridafarrukh <ridafarrukh92@gmail.com>
2020-10-25 00:52:30 +0200

    new line

--- src/lek.java
+++ src/lek.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class lek {
+public class lek {
+    public static void main(String[] args) {
+        System.out.println("hej");
+        System.out.println("rida warsi");
+    }
 }
